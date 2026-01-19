ad12d0da5641171fd08329ab50987489e9479707
LeDutra89 <ledutra3@gmail.com>
2026-01-19 18:43:01 -0300

    commit 01

---
+++ ProjetoCalculadora/src/Projeto/Calculadora.java
@@ -0,0 +1,18 @@
+package Projeto;
+
+import java.util.Scanner;
+
+public class Calculadora {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite Olá Mundo:");
+		String frase = sc.nextLine();
+		
+		sc.close();
+		
+	}
+
+}
